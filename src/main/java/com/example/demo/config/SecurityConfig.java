3151dd077ce6a22e612ddcf2846482e33f4a36c8
Thu Nghia <maymimim.1202@gmail.com>
2024-08-09 02:07:29 +0700

    update SecurityConfig

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,55 +1,57 @@
 package com.example.demo.config;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-//@EnableWebSecurity
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(authorizeRequests -> {
-//                            authorizeRequests
-//                                    .requestMatchers(HttpMethod.GET, "/**").permitAll()
-//                                    .requestMatchers(HttpMethod.PUT, "/update/**").hasRole("USER")
-//                                    .requestMatchers(HttpMethod.GET, "/index").hasRole("USER")
-//                                    .anyRequest().authenticated();
-//                        }
-//                )
-//                .formLogin(withDefaults())
-//                .httpBasic(withDefaults());
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        PasswordEncoder passwordEncoder = passwordEncoder(); // Sử dụng PasswordEncoder bean
-//        String rawPassword = "123@123"; // Mật khẩu gốc
-//        String encodedPassword = passwordEncoder.encode(rawPassword);
-//
-//        return new InMemoryUserDetailsManager(
-//                User.withUsername("HangNT169")
-//                        .password(encodedPassword)
-//                        .roles("USER")
-//                        .build()
-//        );
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests(authorizeRequests -> {
+                            authorizeRequests
+                                    .requestMatchers(HttpMethod.GET, "/index").hasRole("USER")
+                                    .requestMatchers( HttpMethod.GET,"/**").permitAll()
+                                    .requestMatchers(HttpMethod.POST, "/**").permitAll()
+                                    .requestMatchers(HttpMethod.PUT, "/**").permitAll()
+                                    .requestMatchers(HttpMethod.DELETE, "/**").permitAll()
+                                    .anyRequest().authenticated();
+                        }
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        PasswordEncoder passwordEncoder = passwordEncoder(); // Sử dụng PasswordEncoder bean
+        String rawPassword = "123@123"; // Mật khẩu gốc
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
+        return new InMemoryUserDetailsManager(
+                User.withUsername("HangNT169")
+                        .password(encodedPassword)
+                        .roles("USER")
+                        .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
